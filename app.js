b6f5fc6bdcb4770fdbc0052439ed1e75052b6186
Hieu Nguyen <hnnguyen45@gmail.com>
2016-09-19 01:33:56 -0700

    Added JS file to make app work

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function(){
+
+var newContact;
+var list=[];
+var Contact = {
+	firstName : "",
+	lastName : "",
+	phoneNumber : "",
+	street : "",
+	city : "",
+	state : ""
+};
+
+	
