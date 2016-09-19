df68cb1bf822bcd9d6add49f653f3268effda744
Hieu Nguyen <hnnguyen45@gmail.com>
2016-09-19 01:54:33 -0700

    fix a few bugs and add JS file almost completed.

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-
-
-$(document).ready(function(){
-
+//Create defining variables and global before writing jQuery and Javascript.
 var newContact;
 var list=[];
 var Contact = {
@@ -13,4 +10,78 @@ var Contact = {
 	state : ""
 };
 
+$(document).ready(function(){
+
+
+
+	$("#btn-submit").click(function(e){
+		e.preventDefault();
+		createContact();
+		displayContactList();	
 	
+	});
+
+	$('ul').on('click', 'li', function(event) {
+  		contactDetail($(this)[0].id);
+	});
+});
+
+function createContact(){
+	newContact = Object.create(Contact);
+
+	newContact.firstName = $("#firstName").val();
+	newContact.lastName = $("#lastName").val();
+	newContact.phoneNumber = $("#phoneNumber").val();
+	newContact.street = $("#street").val();
+	newContact.city = $("#city").val();
+	newContact.state = $("#state").val();
+
+	list.push(newContact);
+}
+
+function displayContactList(){
+	$(".contact-list ul").append('<a><li id="' + (list.length-1) +'">'+newContact.firstName + ' ' +newContact.lastName+'<br></li></a>');
+	
+	// clear form
+	$("#firstName").val("");
+	$("#lastName").val("");
+	$("#phoneNumber").val("");
+	$("#street").val("");
+	$("#city").val("");
+	$("#state").val("");
+}
+
+function contactDetail(index){
+
+	// clear content
+	$(".firstname").text("");
+	$(".lastname").text("");
+	$(".phone-number").text("");
+	$(".address").text("");
+
+
+	$(".firstname").append("Fist Name: "+list[index].firstName);
+	$(".lastname").append("Last Name: "+list[index].lastName);
+	$(".phone-number").append("Phone Number: "+list[index].phoneNumber);
+	$(".address").append("<p>Address</p>");
+
+
+	if((list[index].street != "") && (list[index].city != "")){
+		$(".address").append("<li>"+list[index].street+", "+list[index].city+"</li>");
+	}else if((list[index].street != "") && (list[index].state != "")){
+		$(".address").append("<li>"+list[index].street+", "+list[index].state+"</li>");
+	}else if((list[index].city != "") && (list[index].state != "")){
+		$(".address").append("<li>"+list[index].city+", "+list[index].state+"</li>");
+	}else if((list[index].street != "") && (list[index].city != "") && (list[index].state != "")){
+		$(".address").append("<li>"+list[index].street+", "+list[index].city+", "+list[index].state+"</li>")
+	}else{
+		if(list[index].street != ""){
+			$(".address").append("<li>"+list[index].street+"</li>");
+		}else if(list[index].city != ""){
+			$(".address").append("<li>"+list[index].city+"</li>");
+		}else if(list[index].state != ""){
+			$(".address").append("<li>"+list[index].state+"</li>");
+		}
+	}
+
+};
